1d10ea90b5036d279cf3844bfea73e2ca48060d8
Brad Martin <bradwaynemartin@gmail.com>
2018-01-05 17:01:48 -0600

    adds template option with string path

--- src/popup.android.ts
+++ src/popup.android.ts
@@ -1,7 +1,10 @@
-import { Common, PopupOptions } from './popup.common';
-import { View } from 'tns-core-modules/ui/core/view';
+import * as builder from 'tns-core-modules/ui/builder';
+import * as fs from 'tns-core-modules/file-system';
 import * as utils from 'tns-core-modules/utils/utils';
 import * as frame from 'tns-core-modules/ui/frame';
+import { Common, PopupOptions } from './popup.common';
+import { View } from 'tns-core-modules/ui/core/view';
+import { StackLayout } from 'tns-core-modules/ui/layouts/stack-layout';
 import { Color } from 'tns-core-modules/color';
 import { screen, device } from 'tns-core-modules/platform';
 export class Popup extends Common {
@@ -28,94 +31,31 @@ export class Popup extends Common {
       this._popup.setOutsideTouchable(true);
       this.reject = reject;
       this.resolve = resolve;
+      // check the view argument
+      debugger;
       if (view instanceof android.view.View) {
         this._popup.setContentView(view);
       } else if (view instanceof View) {
         frame.topmost()._addView(view);
-        let height;
-        let width;
-        switch (this._options.unit) {
-          case 'px':
-            if (this._options.height && !this._options.width) {
-              height = this._options.height;
-              width =
-                this._options.height *
-                (screen.mainScreen.widthPixels /
-                  screen.mainScreen.heightPixels);
-            } else if (this._options.width && !this._options.height) {
-              height =
-                this._options.width *
-                (screen.mainScreen.widthPixels /
-                  screen.mainScreen.heightPixels);
-              width = this._options.width;
-            } else {
-              width = this._options.width;
-              height = this._options.height;
-            }
-            break;
-          case '%':
-            if (this._options.height && !this._options.width) {
-              height =
-                screen.mainScreen.heightDIPs * (this._options.height / 100);
-              width =
-                height *
-                (screen.mainScreen.widthPixels /
-                  screen.mainScreen.heightPixels);
-            } else if (this._options.width && !this._options.height) {
-              width = screen.mainScreen.widthDIPs * (this._options.width / 100);
-              height =
-                width *
-                (screen.mainScreen.widthPixels /
-                  screen.mainScreen.heightPixels);
-            } else {
-              width = screen.mainScreen.widthDIPs * (this._options.width / 100);
-              height =
-                screen.mainScreen.heightDIPs * (this._options.height / 100);
-            }
-            break;
-          default:
-            if (this._options.height && !this._options.width) {
-              height = this._options.height;
-              width =
-                this._options.height *
-                (screen.mainScreen.widthPixels /
-                  screen.mainScreen.heightPixels);
-            } else if (this._options.width && !this._options.height) {
-              height =
-                this._options.width *
-                (screen.mainScreen.widthPixels /
-                  screen.mainScreen.heightPixels);
-              width = this._options.width;
-            } else {
-              width = this._options.width ? this._options.width : 400;
-              height = this._options.height ? this._options.height : 320;
-            }
-            break;
-        }
-
-        const shape = new android.graphics.drawable.GradientDrawable();
-        shape.setShape(android.graphics.drawable.GradientDrawable.RECTANGLE);
-
-        if (this._options && this._options.borderRadius) {
-          shape.setCornerRadius(this._options.borderRadius);
-        }
-        if (this._options && this._options.backgroundColor) {
-          shape.setColor(
-            new Color(this._options.backgroundColor).android
-          );
-        }
-        (this._popup as any).setBackgroundDrawable(shape);
-        if (parseInt(device.sdkVersion, 10) >= 21) {
-          if (this._options.elevation) {
-            (this._popup as any).setElevation(this._options.elevation);
-          }
-        }
-        this._popup.setWidth(utils.layout.toDevicePixels(width));
-        this._popup.setHeight(utils.layout.toDevicePixels(height));
-
+        this._stylePopup()
         this._view = view;
         this._popup.setContentView(view.android);
+      } else if (typeof view == 'string' || view instanceof String) {
+        // this is a template so use the builder to load the template
+        this._stylePopup();
+        const stack = new StackLayout();
+        frame.topmost()._addView(stack);
+        stack.removeChildren(); // ensure nothing in the stack
+        const path = fs.knownFolders.currentApp().path;
+        const component = builder.load(
+            path + view
+        );
+        stack.addChild(component);
+        this._view = stack;
+        this._popup.setContentView(stack.android);
       }
+
+      // check the source argument
       if (source instanceof android.view.View) {
         this._popup.showAsDropDown(source);
       } else if (source instanceof View) {
@@ -131,5 +71,88 @@ export class Popup extends Common {
     if (this._view) {
       frame.topmost()._removeView(this._view);
     }
+  }
+
+  private _stylePopup() {
+    let height;
+    let width;
+    switch (this._options.unit) {
+      case 'px':
+        if (this._options.height && !this._options.width) {
+          height = this._options.height;
+          width =
+            this._options.height *
+            (screen.mainScreen.widthPixels /
+              screen.mainScreen.heightPixels);
+        } else if (this._options.width && !this._options.height) {
+          height =
+            this._options.width *
+            (screen.mainScreen.widthPixels /
+              screen.mainScreen.heightPixels);
+          width = this._options.width;
+        } else {
+          width = this._options.width;
+          height = this._options.height;
+        }
+        break;
+      case '%':
+        if (this._options.height && !this._options.width) {
+          height =
+            screen.mainScreen.heightDIPs * (this._options.height / 100);
+          width =
+            height *
+            (screen.mainScreen.widthPixels /
+              screen.mainScreen.heightPixels);
+        } else if (this._options.width && !this._options.height) {
+          width = screen.mainScreen.widthDIPs * (this._options.width / 100);
+          height =
+            width *
+            (screen.mainScreen.widthPixels /
+              screen.mainScreen.heightPixels);
+        } else {
+          width = screen.mainScreen.widthDIPs * (this._options.width / 100);
+          height =
+            screen.mainScreen.heightDIPs * (this._options.height / 100);
+        }
+        break;
+      default:
+        if (this._options.height && !this._options.width) {
+          height = this._options.height;
+          width =
+            this._options.height *
+            (screen.mainScreen.widthPixels /
+              screen.mainScreen.heightPixels);
+        } else if (this._options.width && !this._options.height) {
+          height =
+            this._options.width *
+            (screen.mainScreen.widthPixels /
+              screen.mainScreen.heightPixels);
+          width = this._options.width;
+        } else {
+          width = this._options.width ? this._options.width : 400;
+          height = this._options.height ? this._options.height : 320;
+        }
+        break;
+    }
+
+    const shape = new android.graphics.drawable.GradientDrawable();
+    shape.setShape(android.graphics.drawable.GradientDrawable.RECTANGLE);
+
+    if (this._options && this._options.borderRadius) {
+      shape.setCornerRadius(this._options.borderRadius);
+    }
+    if (this._options && this._options.backgroundColor) {
+      shape.setColor(
+        new Color(this._options.backgroundColor).android
+      );
+    }
+    (this._popup as any).setBackgroundDrawable(shape);
+    if (parseInt(device.sdkVersion, 10) >= 21) {
+      if (this._options.elevation) {
+        (this._popup as any).setElevation(this._options.elevation);
+      }
+    }
+    this._popup.setWidth(utils.layout.toDevicePixels(width));
+    this._popup.setHeight(utils.layout.toDevicePixels(height));
   }
 }
